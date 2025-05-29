d4032ef1812f99f801e591fa1c8055084129894a
Caio Wilker <caiowilkerlima31@gmail.com>
2025-05-28 21:54:15 -0300

    feat: Primeiro commmit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public Main(String[] args) {
+
+
+        System.out.println("Hello, World.");
+
+    }
+}
---
+++ src/main/java/org/example/bank/Util.java
@@ -0,0 +1,17 @@
+package org.example.bank;public class Util {
+    private Model account;
+
+    public Util(Model account) {
+        this.account = account;
+    }
+
+    public void deposit(double valor) {
+        valor = valor + account.getSaldo();
+        account.setSaldo(valor);
+    }
+
+    public void sacar(double valor) {
+        valor = account.getSaldo() - valor - account.getTax();
+        account.setSaldo(valor);
+    }
+}
