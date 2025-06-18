6e1a5b41db79f558a8ce6ad3696dae0f5256fcec
Caio Wilker <caiowilkerlima31@gmail.com>
2025-06-18 20:22:30 -0300

    Reusing instantiation

--- src/main/java/org/example/jdbc1/demo_dao_jdbc/application/Program.java
+++ src/main/java/org/example/jdbc1/demo_dao_jdbc/application/Program.java
@@ -1,8 +1,17 @@
 package org.example.jdbc1.demo_dao_jdbc.application;
 
+import org.example.jdbc1.demo_dao_jdbc.model.dao.DaoFactory;
+import org.example.jdbc1.demo_dao_jdbc.model.dao.SellerDao;
+import org.example.jdbc1.demo_dao_jdbc.model.entities.Seller;
+
 public class Program {
 
     public static void main(String[] args) {
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("===== Test 1: seller findById =====");
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
